24f56562a46f12da83eced99f4cbd21f0aaaa129
brian d foy <brian.d.foy@gmail.com>
2013-07-22 20:05:47 -0400

    initial creation;

---
+++ lib/ReturnValue.pm
@@ -0,0 +1,71 @@
+package ReturnValue;
+use strict;
+
+use warnings;
+no warnings;
+
+use subs qw();
+use vars qw($VERSION);
+
+$VERSION = '0.10_01';
+
+=encoding utf8
+
+=head1 NAME
+
+ReturnValue - 
+
+=head1 SYNOPSIS
+
+	use ReturnValue;
+
+=head1 DESCRIPTION
+
+=over 4
+
+=item new
+
+=cut
+
+sub new
+	{
+	
+	}
+	
+=item init
+
+=cut
+
+sub init
+	{
+	
+	}
+
+=back
+
+=head1 TO DO
+
+
+=head1 SEE ALSO
+
+
+=head1 SOURCE AVAILABILITY
+
+This source is in Github:
+
+	http://github.com/briandfoy/returnvalue/
+
+=head1 AUTHOR
+
+, C<< <> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+
+Copyright (c) 2013, brian d foy, All Rights Reserved.
+
+You may redistribute this under the same terms as Perl itself.
+
+=cut
+
+1;
