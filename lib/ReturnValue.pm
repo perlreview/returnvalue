f11974f0177cf876c0b5d95a79581266722420ce
brian d foy <brian.d.foy@gmail.com>
2015-01-03 17:05:46 -0500

    Update copyright statement

--- lib/ReturnValue.pm
+++ lib/ReturnValue.pm
@@ -188,7 +188,7 @@ brian d foy, C<< <bdfoy@cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2013, brian d foy, All Rights Reserved.
+Copyright © 2013-2015, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 You may redistribute this under the same terms as Perl itself.
 
