b1683008e194a90c62068d89dd209a2e828fb4fa
brian d foy <brian.d.foy@gmail.com>
2019-07-14 23:41:10 -0400

    Update the year

--- lib/ReturnValue.pm
+++ lib/ReturnValue.pm
@@ -186,7 +186,7 @@ brian d foy, <bdfoy@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2013-2018, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2013-2019, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 You may redistribute this under the terms of the Artistic License 2.0.
 
